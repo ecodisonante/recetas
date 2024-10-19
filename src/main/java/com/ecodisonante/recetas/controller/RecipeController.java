b02ec944aed054b59d8c18af32f549d2c6dfb692
Francisco Valdes <ecodisonante@gmail.com>
2024-10-19 19:49:39 -0300

    pagina receta 1 estatica

---
+++ src/main/java/com/ecodisonante/recetas/controller/RecipeController.java
@@ -0,0 +1,22 @@
+package com.ecodisonante.recetas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RecipeController {
+
+    public static final String TITLE = "La Abuela Digital";
+
+    //TODO: Reemplazar por id dinammico
+    @GetMapping("/recipe/1")
+    public String home(
+            @RequestParam(name = "name", required = false, defaultValue = TITLE) String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "Recipe1";
+    }
+
+}
