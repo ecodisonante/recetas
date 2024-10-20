e5adf37cb3ae27b649ba5d7df4d6dc6cbc1fdccc
Francisco Valdes <ecodisonante@gmail.com>
2024-10-19 21:11:41 -0300

    usar plantilla y cargar contenido dinamico

--- src/main/java/com/ecodisonante/recetas/controller/HomeController.java
+++ src/main/java/com/ecodisonante/recetas/controller/HomeController.java
@@ -8,20 +8,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
+    public static final String TITLE = "La Abuela Digital";
+
     @GetMapping("/home")
     public String home(
-            @RequestParam(name = "name", required = false, defaultValue = "Seguridad y Calidad en el Desarrollo") String name,
+            @RequestParam(name = "name", required = false, defaultValue = TITLE) String name,
             Model model) {
         model.addAttribute("name", name);
-        return "Home";
+        // Establecer el título y fragmento de contenido para la página de login
+        model.addAttribute("content", "Home"); // Se refiere al template login.html
+        return "layout"; // usar plantilla "layout.html" como base
     }
 
     @GetMapping("/")
     public String root(
-            @RequestParam(name = "name", required = false, defaultValue = "Seguridad y Calidad en el Desarrollo") String name,
+            @RequestParam(name = "name", required = false, defaultValue = TITLE) String name,
             Model model) {
         model.addAttribute("name", name);
-        return "Home";
+        // Establecer el título y fragmento de contenido para la página de login
+        model.addAttribute("content", "Home"); // Se refiere al template login.html
+        return "layout"; // usar plantilla "layout.html" como base
     }
 
 }
