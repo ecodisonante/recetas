d76548f5fa7fd1f75ec3bfeb3f931ab5f3a5190f
Francisco Valdes <ecodisonante@gmail.com>
2024-10-21 00:23:55 -0300

    Carga dinamica en vista Recipe

--- src/main/java/com/ecodisonante/recetas/controller/RecipeController.java
+++ src/main/java/com/ecodisonante/recetas/controller/RecipeController.java
@@ -3,7 +3,9 @@ package com.ecodisonante.recetas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ecodisonante.recetas.model.DataLoader;
 
 @Controller
 public class RecipeController {
@@ -11,11 +13,14 @@ public class RecipeController {
     public static final String TITLE = "La Abuela Digital";
 
     // TODO: Reemplazar por id dinammico
-    @GetMapping("/recipe/1")
-    public String home(
-            @RequestParam(name = "name", required = false, defaultValue = TITLE) String name,
-            Model model) {
-        model.addAttribute("content", "Recipe1"); // Se refiere al template login.html
+    @GetMapping("/recipe/{id}")
+    public String getRecetaById(@PathVariable("id") Long id, Model model) {
+        // Buscar receta
+        var recipe = DataLoader.loadRecipes().stream().filter(x -> x.getId().equals(id)).findFirst();
+        // Añadir la receta al modelo
+        model.addAttribute("recipe", recipe.get());
+        model.addAttribute("content", "Recipe"); // Se refiere al template recipe1.html
+
         return "layout"; // usar plantilla "layout.html" como base
     }
 
