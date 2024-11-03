c5c1226614e798a5b15b09392a6ed57af36d452e
Francisco Valdes <ecodisonante@gmail.com>
2024-11-02 21:26:45 -0300

    recipe consumiendo service

--- src/main/java/com/ecodisonante/recetas/controller/RecipeController.java
+++ src/main/java/com/ecodisonante/recetas/controller/RecipeController.java
@@ -1,25 +1,30 @@
 package com.ecodisonante.recetas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.ecodisonante.recetas.model.DataLoader;
+import com.ecodisonante.recetas.service.RecipeService;
 
 @Controller
 public class RecipeController {
 
+    @Autowired
+    private RecipeService service;
+
     public static final String TITLE = "La Abuela Digital";
 
-    // TODO: Reemplazar por id dinammico
     @GetMapping("/recipe/{id}")
     public String getRecetaById(@PathVariable("id") Long id, Model model) {
+        
         // Buscar receta
-        var recipe = DataLoader.loadRecipes().stream().filter(x -> x.getId().equals(id)).findFirst();
+        var recipe = service.getById(id);
+
         // Añadir la receta al modelo
-        model.addAttribute("recipe", recipe.get());
-        model.addAttribute("content", "Recipe"); // Se refiere al template recipe1.html
+        model.addAttribute("recipe", recipe);
+        model.addAttribute("content", "Recipe"); 
 
         return "layout"; // usar plantilla "layout.html" como base
     }
